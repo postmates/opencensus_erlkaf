bfb660efdab76ba1861002e93c4d0e8812a0b4eb
Tristan Sloughter <t@crashfast.com>
2019-06-03 11:05:50 -0600

    record delivery error counts and log

--- src/opencensus_erlkaf.erl
+++ src/opencensus_erlkaf.erl
@@ -8,6 +8,7 @@
          default_views/0]).
 
 -include_lib("erlkaf/include/erlkaf.hrl").
+-include_lib("kernel/include/logger.hrl").
 
 %% Based on the stats found here:
 %% https://github.com/postmates/pmpy/blob/master/pmpy/kafka/stats.py
@@ -24,7 +25,15 @@ stats_callback(_ClientId, #{<<"type">> := Type,
     ok.
 
 -spec delivery_report(ok | {error, any()}, #erlkaf_msg{}) -> ok.
-delivery_report(_DeliveryStatus, _Message) ->
+delivery_report({error, Reason}, #erlkaf_msg{topic=Topic,
+                                             partition=Partition}) ->
+    ?LOG_ERROR("error publishing message to topic=~s partition=~p reason=~p", [Topic,
+                                                                               Partition,
+                                                                               Reason]),
+    oc_stat:record(#{type => <<"producer">>,
+                     topic => Topic,
+                     partition => Partition}, [{'rdkafka/partition/delivery_error', 1}]);
+delivery_report(ok, _Message) ->
     ok.
 
 register_and_subscribe() ->
@@ -36,7 +45,12 @@ register_measures() ->
     [oc_stat_measure:new(Name, Description, Unit) || {Name, Description, Unit} <- measurements()].
 
 default_views() ->
-    views(topic, ?TOPIC_TAGS) ++ views(partition, ?PARTITION_TAGS).
+    [#{name => "rdkafka/partition/delivery_error",
+       description => "Number of errors reported for delivery attempts",
+       tags => [partition | ?TOPIC_TAGS],
+       measure => 'rdkafka/partition/delivery_error',
+       aggregation => oc_stat_aggregation_count}
+     | views(topic, ?TOPIC_TAGS) ++ views(partition, ?PARTITION_TAGS)].
 
 views(Type, Tags) ->
     [#{name => atom_to_list(Name),
@@ -71,29 +85,29 @@ process_topic(Type, Topic, #{<<"batchsize">> :=
                                    <<"p99_99">> := CntP99_99},
                              <<"partitions">> := Partitions}) ->
     oc_stat:record(#{type => Type, topic => Topic},
-                   [{'kafka/topic/batchsize/min', SizeMin},
-                    {'kafka/topic/batchsize/max', SizeMax},
-                    {'kafka/topic/batchsize/avg', SizeAvg},
-                    {'kafka/topic/batchsize/sum', SizeSum},
-                    {'kafka/topic/batchsize/cnt', SizeCnt},
-                    {'kafka/topic/batchsize/stddev', SizeStdDev},
-                    {'kafka/topic/batchsize/p50', SizeP50},
-                    {'kafka/topic/batchsize/p75', SizeP75},
-                    {'kafka/topic/batchsize/p90', SizeP90},
-                    {'kafka/topic/batchsize/p99', SizeP99},
-                    {'kafka/topic/batchsize/p99_99', SizeP99_99},
+                   [{'rdkafka/topic/batchsize/min', SizeMin},
+                    {'rdkafka/topic/batchsize/max', SizeMax},
+                    {'rdkafka/topic/batchsize/avg', SizeAvg},
+                    {'rdkafka/topic/batchsize/sum', SizeSum},
+                    {'rdkafka/topic/batchsize/cnt', SizeCnt},
+                    {'rdkafka/topic/batchsize/stddev', SizeStdDev},
+                    {'rdkafka/topic/batchsize/p50', SizeP50},
+                    {'rdkafka/topic/batchsize/p75', SizeP75},
+                    {'rdkafka/topic/batchsize/p90', SizeP90},
+                    {'rdkafka/topic/batchsize/p99', SizeP99},
+                    {'rdkafka/topic/batchsize/p99_99', SizeP99_99},
 
-                    {'kafka/topic/batchcnt/min', CntMin},
-                    {'kafka/topic/batchcnt/max', CntMax},
-                    {'kafka/topic/batchcnt/avg', CntAvg},
-                    {'kafka/topic/batchcnt/sum', CntSum},
-                    {'kafka/topic/batchcnt/cnt', CntCnt},
-                    {'kafka/topic/batchcnt/stddev', CntStdDev},
-                    {'kafka/topic/batchcnt/p50', CntP50},
-                    {'kafka/topic/batchcnt/p75', CntP75},
-                    {'kafka/topic/batchcnt/p90', CntP90},
-                    {'kafka/topic/batchcnt/p99', CntP99},
-                    {'kafka/topic/batchcnt/p99_99', CntP99_99}]),
+                    {'rdkafka/topic/batchcnt/min', CntMin},
+                    {'rdkafka/topic/batchcnt/max', CntMax},
+                    {'rdkafka/topic/batchcnt/avg', CntAvg},
+                    {'rdkafka/topic/batchcnt/sum', CntSum},
+                    {'rdkafka/topic/batchcnt/cnt', CntCnt},
+                    {'rdkafka/topic/batchcnt/stddev', CntStdDev},
+                    {'rdkafka/topic/batchcnt/p50', CntP50},
+                    {'rdkafka/topic/batchcnt/p75', CntP75},
+                    {'rdkafka/topic/batchcnt/p90', CntP90},
+                    {'rdkafka/topic/batchcnt/p99', CntP99},
+                    {'rdkafka/topic/batchcnt/p99_99', CntP99_99}]),
     maps:map(fun(Partition, Stats) ->
                  process_partition(Type, Topic, Partition, Stats)
              end, Partitions).
@@ -113,49 +127,50 @@ process_partition(Type, Topic, Partition, #{<<"leader">> := Leader,
                      fetch_state => FetchState,
                      type => Type,
                      topic => Topic},
-                   [{'kafka/partition/fetchq_cnt', FetchQCnt},
-                    {'kafka/partition/fetchq_size', FetchQSize},
-                    {'kafka/partition/consumer_lag', ConsumerLag},
-                    {'kafka/partition/txmsgs', TxMsgs},
-                    {'kafka/partition/txbytes', TxBytes},
-                    {'kafka/partition/rxmsgs', RxMsgs},
-                    {'kafka/partition/rxbytes', RxBytes},
-                    {'kafka/partition/msgs', Msgs}]).
+                   [{'rdkafka/partition/fetchq_cnt', FetchQCnt},
+                    {'rdkafka/partition/fetchq_size', FetchQSize},
+                    {'rdkafka/partition/consumer_lag', ConsumerLag},
+                    {'rdkafka/partition/txmsgs', TxMsgs},
+                    {'rdkafka/partition/txbytes', TxBytes},
+                    {'rdkafka/partition/rxmsgs', RxMsgs},
+                    {'rdkafka/partition/rxbytes', RxBytes},
+                    {'rdkafka/partition/msgs', Msgs}]).
 measurements() ->
-    measurements(topic) ++ measurements(partition).
+    [{'rdkafka/partition/delivery_error', "Number of errors reported for delivery attempts", none}
+     | measurements(topic) ++ measurements(partition)].
 
 measurements(topic) ->
-    [{'kafka/topic/batchsize/min', "Smallest batch size", bytes},
-     {'kafka/topic/batchsize/max', "Largest batch size", bytes},
-     {'kafka/topic/batchsize/avg', "Average batch size", bytes},
-     {'kafka/topic/batchsize/sum', "Sum of batch sizes", bytes},
-     {'kafka/topic/batchsize/cnt', "Number of batch sizes sampled", none},
-     {'kafka/topic/batchsize/stddev', "Standard deviation of batch sizes", bytes},
-     {'kafka/topic/batchsize/p50', "Batch size 50th percentile", bytes},
-     {'kafka/topic/batchsize/p75', "Batch size 75th percentile", bytes},
-     {'kafka/topic/batchsize/p90', "Batch size 90th percentile", bytes},
-     {'kafka/topic/batchsize/p99', "Batch size 99th percentile", bytes},
-     {'kafka/topic/batchsize/p99_99', "Batch size 99.99th percentile", bytes},
-     {'kafka/topic/batchcnt/min', "Smallest batch message count", bytes},
-     {'kafka/topic/batchcnt/max', "Largest batch message count", bytes},
-     {'kafka/topic/batchcnt/avg', "Average batch message count", bytes},
-     {'kafka/topic/batchcnt/sum', "Sum of batch message counts", bytes},
-     {'kafka/topic/batchcnt/cnt', "Number of batch message counts sampled", none},
-     {'kafka/topic/batchcnt/stddev', "Standard deviation of batch message counts", bytes},
-     {'kafka/topic/batchcnt/p50', "batch message count 50th percentile", bytes},
-     {'kafka/topic/batchcnt/p75', "batch message count 75th percentile", bytes},
-     {'kafka/topic/batchcnt/p90', "batch message count 90th percentile", bytes},
-     {'kafka/topic/batchcnt/p99', "batch message count 99th percentile", bytes},
-     {'kafka/topic/batchcnt/p99_99', "batch message count 99.99th percentile", bytes}];
+    [{'rdkafka/topic/batchsize/min', "Smallest batch size", bytes},
+     {'rdkafka/topic/batchsize/max', "Largest batch size", bytes},
+     {'rdkafka/topic/batchsize/avg', "Average batch size", bytes},
+     {'rdkafka/topic/batchsize/sum', "Sum of batch sizes", bytes},
+     {'rdkafka/topic/batchsize/cnt', "Number of batch sizes sampled", none},
+     {'rdkafka/topic/batchsize/stddev', "Standard deviation of batch sizes", bytes},
+     {'rdkafka/topic/batchsize/p50', "Batch size 50th percentile", bytes},
+     {'rdkafka/topic/batchsize/p75', "Batch size 75th percentile", bytes},
+     {'rdkafka/topic/batchsize/p90', "Batch size 90th percentile", bytes},
+     {'rdkafka/topic/batchsize/p99', "Batch size 99th percentile", bytes},
+     {'rdkafka/topic/batchsize/p99_99', "Batch size 99.99th percentile", bytes},
+     {'rdkafka/topic/batchcnt/min', "Smallest batch message count", bytes},
+     {'rdkafka/topic/batchcnt/max', "Largest batch message count", bytes},
+     {'rdkafka/topic/batchcnt/avg', "Average batch message count", bytes},
+     {'rdkafka/topic/batchcnt/sum', "Sum of batch message counts", bytes},
+     {'rdkafka/topic/batchcnt/cnt', "Number of batch message counts sampled", none},
+     {'rdkafka/topic/batchcnt/stddev', "Standard deviation of batch message counts", bytes},
+     {'rdkafka/topic/batchcnt/p50', "batch message count 50th percentile", bytes},
+     {'rdkafka/topic/batchcnt/p75', "batch message count 75th percentile", bytes},
+     {'rdkafka/topic/batchcnt/p90', "batch message count 90th percentile", bytes},
+     {'rdkafka/topic/batchcnt/p99', "batch message count 99th percentile", bytes},
+     {'rdkafka/topic/batchcnt/p99_99', "batch message count 99.99th percentile", bytes}];
 measurements(partition) ->
-    [{'kafka/partition/fetchq_cnt', "Number of pre-fetched messages in fetch queue", none},
-     {'kafka/partition/fetchq_size', "Bytes in fetchq", bytes},
-     {'kafka/partition/consumer_lag',
+    [{'rdkafka/partition/fetchq_cnt', "Number of pre-fetched messages in fetch queue", none},
+     {'rdkafka/partition/fetchq_size', "Bytes in fetchq", bytes},
+     {'rdkafka/partition/consumer_lag',
       "Difference between hi_offset - max(app_offset, committed_offset", none},
-     {'kafka/partition/txmsgs', "Total number of messages transmitted (produced)", bytes},
-     {'kafka/partition/txbytes', "Total number of bytes transmitted for txmsgs", bytes},
-     {'kafka/partition/rxmsgs',
+     {'rdkafka/partition/txmsgs', "Total number of messages transmitted (produced)", bytes},
+     {'rdkafka/partition/txbytes', "Total number of bytes transmitted for txmsgs", bytes},
+     {'rdkafka/partition/rxmsgs',
       "Total number of messages consumed, not including ignored messages (due to offset, etc).", none},
-     {'kafka/partition/rxbytes', "Total number of bytes received for rxmsgs", bytes},
-     {'kafka/partition/msgs', "Total number of messages received (consumer, same as rxmsgs), "
+     {'rdkafka/partition/rxbytes', "Total number of bytes received for rxmsgs", bytes},
+     {'rdkafka/partition/msgs', "Total number of messages received (consumer, same as rxmsgs), "
       "or total number of messages produced (possibly not yet transmitted) (producer).", none}].
